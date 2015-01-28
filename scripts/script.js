923e5a84d5500fff24fb77f31cd306cafafe0777
Tyler Berry <tyler@berryac.com>
2015-01-28 16:44:39 -0700

    Got the first formatted date to appear in the content of the first accordion. I will probably need unique ID's for everything... sigh...

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,8 @@
+////////////////////// Mr. Beatty's code ////////////////////////
+
 var weatherData;
 var request = new XMLHttpRequest();
 var date = new Date();
-
-function getDayOfWeek(num) {
-    switch(num) {
-        case 0: 
-            return 'Sunday';
-        case 1: 
-            return 'Monday';
-        case 2: 
-            return 'Tuesday';
-        case 3: 
-            return 'Wednesday';
-        case 4: 
-            return 'Thursday';
-        case 5: 
-            return 'Friday';
-        case 6: 
-            return 'Saturday';
-    }
-}
-
-function fillAccordionHeaders() {    
-    for(var i = 0; i < 7; i++) {
-        var dayNumber = date.getDay() + i;
-        if(dayNumber >= 7) dayNumber -= 7;
-        document.getElementById('day' + i + 'header').innerHTML = getDayOfWeek(dayNumber);
-    }
-}
-
-fillAccordionHeaders();
-
-////////////////////// Mr. Beatty's code ////////////////////////
 
 loadData();
 
@@ -50,3 +21,38 @@ function loadComplete(evt) {
     document.getElementById("conditions").innerHTML = weatherData.list[0].weather[0].main;
     document.getElementById("conditionsDesc").innerHTML = weatherData.list[0].weather[0].description;  
 }
+
+////////////////////// Our code ////////////////////////
+
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// Sets the date to tomorrow. Does all month adjusting for you.
+//        date.setDate(date.getDate() + 1);
+//        formatDate(date);
+
+function formatDate(rawDate) {
+    return months[rawDate.getMonth()] + ' ' + rawDate.getDate() + ', ' + rawDate.getFullYear(); // reads as January 28, 2015
+}
+
+function fillAccordionHeaders() {
+    for(var i = 0; i < 7; i++) {
+        // Headers
+        var dayNumber = date.getDay() + i;
+        if(dayNumber >= 7) dayNumber -= 7;
+        document.getElementById('day' + i + 'header').innerHTML = days[dayNumber];
+        
+        // Contents
+        document.getElementById('date').innerHTML = formatDate(date);
+    }
+}
+
+function fillAccordionContent() {
+    for(var i = 0; i < 7; i++) {
+        
+    }
+}
+
+fillAccordionHeaders();
+fillAccordionContent();
+
